38ffd9c9d40ab11661dc67476a0ed44543802f9b
DmrLutfullin <dmr.lutfullin@gmail.com>
2020-01-05 18:51:31 +0500

    about Xcode

---
+++ ObjcLesson1/AnyClass.h
@@ -0,0 +1,17 @@
+//
+//  AnyClass.h
+//  ObjcLesson1
+//
+//  Created by Дамир Лутфуллин on 05.01.2020.
+//  Copyright © 2020 Damir Lutfullin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AnyClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjcLesson1/AnyClass.m
@@ -0,0 +1,13 @@
+//
+//  AnyClass.m
+//  ObjcLesson1
+//
+//  Created by Дамир Лутфуллин on 05.01.2020.
+//  Copyright © 2020 Damir Lutfullin. All rights reserved.
+//
+
+#import "AnyClass.h"
+
+@implementation AnyClass
+
+@end
